bbcb77cf37a3387dd7065d299f9d4e728480db9a
ihsnokn <ihsanokan38@gmail.com>
2022-08-22 15:35:20 +0300

    Game commit
    first commit.

---
+++ js/words.js
@@ -0,0 +1,24 @@
+let words = [
+    {
+        word: "GOAT",
+        hint: "Greatest of All Time"
+    },
+    {
+        word: "meeting",
+        hint: "event in which people come together"
+    },
+    {
+        word: "nefarious",
+        hint: "Wicked or criminal"
+    },
+    {
+        word: "copperfield",
+        hint: "famous magician"
+    },
+    
+    {
+        word: "lion king",
+        hint: "hakuna matata"
+    },
+    
+]
